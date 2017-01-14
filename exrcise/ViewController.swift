1e6f314889b1cfaef860be23812e0794c1b5d721
Gabriel Erdelyi <erdgabios@gmail.com>
2017-01-14 17:26:52 +0100

    final

--- exrcise/ViewController.swift
+++ exrcise/ViewController.swift
@@ -9,16 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var timer = Timer()
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateLabel), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateLabel() {
+        
+        let dateFormatter = DateFormatter()
+        let timeFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = .full
+        timeFormatter.timeStyle = .full
+        
+        dateLabel.text = dateFormatter.string(from: NSDate() as Date)
+        timeLabel.text = timeFormatter.string(from: NSDate() as Date)
+        
+    }
+    
 
 
 }
